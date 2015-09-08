04762f1e2d8fe2f1c3fb4665583f71fae5911084
Yi Cheng <singye888@gmail.com>
2015-09-08 12:56:49 -0400

    Fix makefile

--- include/user.h
+++ include/user.h
@@ -1,7 +1,22 @@
 #ifndef __USER_H__
 #define __USER_H__
+#include <vector>
+struct Session{
+    double start;
+    double end;
+};
 
 class User {
+  public:
+    const vector<Session> &get_sessions() const{
+        return _visit_session;
+    }
+    const long id() const{
+        return user_id;
+    }
+  private:
+    long user_id;
+    vector<Session> _visit_session;
 };
 typedef std::vector<user> UserArray;
 #endif
