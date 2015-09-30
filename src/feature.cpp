9712e64597cb3cbe3c96635fa788d333efa96fcb
Yi Cheng <singye888@gmail.com>
2015-09-30 17:27:14 -0400

    Remove lock

--- src/feature.cpp
+++ src/feature.cpp
@@ -10,21 +10,20 @@ typedef std::unordered_map<int, std::string> MapFeature;
 FeatureMap ffmap;
 MapFeature mmap;
 int getFeatureOffset(std::string &name){
-    {
-        boost::shared_lock<boost::shared_mutex> lock(rw_lock);
+    //    {
+    //        boost::shared_lock<boost::shared_mutex> lock(rw_lock);
         if(ffmap.count(name))
             return ffmap[name];
-    }
-    {
-        boost::lock_guard<boost::shared_mutex> lock(rw_lock);
+        //    }
+        //    {
+        //        boost::lock_guard<boost::shared_mutex> lock(rw_lock);
         return ffmap[name] = ffmap.size();
-    }
+        //    }
 }
 
 int getFeatureNumber(){
-    {
-        boost::shared_lock<boost::shared_mutex> lock(rw_lock);
+    //    {
+    //        boost::shared_lock<boost::shared_mutex> lock(rw_lock);
         return ffmap.size();
-    }
-
+        //    }
 }
